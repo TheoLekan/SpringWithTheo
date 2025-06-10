efdeeafb1811c9fc00adcd267cee4b853c8cc5b8
theolekan <tijesuolalekan@gmail.com>
2025-06-10 02:55:27 +0100

    Create Basic Project with Spring Initializer

---
+++ Week 1/basics/src/test/java/com/springwithTheo/week1/basics/BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwithTheo.week1.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
