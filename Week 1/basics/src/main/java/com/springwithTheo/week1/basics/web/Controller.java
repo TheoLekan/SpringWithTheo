429595ddbe6d0831bf87f2746968a112d277b859
theolekan <tijesuolalekan@gmail.com>
2025-06-10 03:15:46 +0100

    Implemented a Controller

---
+++ Week 1/basics/src/main/java/com/springwithTheo/week1/basics/web/Controller.java
@@ -0,0 +1,13 @@
+package com.springwithTheo.week1.basics.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String greeting(){
+        return "Welcome Spring With Theo. It's Week 1 we're starting strong!!!";
+    }
+}
