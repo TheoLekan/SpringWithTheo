a6a6e0aecc32ff12f851877e66897e66cf1ef360
theolekan <tijesuolalekan@gmail.com>
2025-06-12 08:42:09 +0100

    Printing Spring Beans in Ctx

--- Week 1/basics/src/main/java/com/springwithTheo/week1/basics/BasicsApplication.java
+++ Week 1/basics/src/main/java/com/springwithTheo/week1/basics/BasicsApplication.java
@@ -2,12 +2,17 @@ package com.springwithTheo.week1.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicsApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(BasicsApplication.class, args);
+		System.out.println(Arrays.toString(ctx.getBeanDefinitionNames()));
 	}
 
 }
