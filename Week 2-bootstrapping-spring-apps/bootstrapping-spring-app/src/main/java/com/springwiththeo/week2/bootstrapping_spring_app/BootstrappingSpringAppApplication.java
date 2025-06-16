d0de7936688414614ec6217af03a83f50c1c3cc3
theolekan <tijesuolalekan@gmail.com>
2025-06-16 20:45:05 +0100

    Initializing week 2 project file
    closes #12

---
+++ Week 2-bootstrapping-spring-apps/bootstrapping-spring-app/src/main/java/com/springwiththeo/week2/bootstrapping_spring_app/BootstrappingSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week2.bootstrapping_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrappingSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrappingSpringAppApplication.class, args);
+	}
+
+}
---
+++ Week 2-bootstrapping-spring-apps/bootstrapping-spring-app/src/test/java/com/springwiththeo/week2/bootstrapping_spring_app/BootstrappingSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week2.bootstrapping_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrappingSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
