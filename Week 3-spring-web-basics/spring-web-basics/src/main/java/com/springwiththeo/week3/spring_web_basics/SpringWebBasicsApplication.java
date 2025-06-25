36e1719f5afe0e57c034e8bf54337f1b619ca130
theolekan <tijesuolalekan@gmail.com>
2025-06-25 09:26:40 +0100

    Week 3 Project Set up
    This application use Spring JPA, Web and H2 starter

---
+++ Week 3-spring-web-basics/spring-web-basics/src/main/java/com/springwiththeo/week3/spring_web_basics/SpringWebBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week3.spring_web_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebBasicsApplication.class, args);
+	}
+
+}
