0f17dc49c4d933ced11cd7182b41a898b312300f
theolekan <tijesuolalekan@gmail.com>
2025-06-26 23:33:34 +0100

    Implement CommandLineRunner to initialize database with sample employees

--- Week 3-spring-web-basics/spring-web-basics/src/main/java/com/springwiththeo/week3/spring_web_basics/SpringWebBasicsApplication.java
+++ Week 3-spring-web-basics/spring-web-basics/src/main/java/com/springwiththeo/week3/spring_web_basics/SpringWebBasicsApplication.java
@@ -1,13 +1,34 @@
 package com.springwiththeo.week3.spring_web_basics;
 
+import com.springwiththeo.week3.spring_web_basics.model.Employee;
+import com.springwiththeo.week3.spring_web_basics.repository.EmployeeRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringWebBasicsApplication {
+import java.util.List;
 
+@SpringBootApplication
+public class SpringWebBasicsApplication implements CommandLineRunner {
+
+	private final EmployeeRepo employeeRepo;
+
+	public SpringWebBasicsApplication(EmployeeRepo employeeRepo) {
+		this.employeeRepo = employeeRepo;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebBasicsApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) {
+		// Initialize the database with some sample data
+		List<Employee> employees = List.of(
+				new Employee("John Doe", "Software Engineer"),
+				new Employee("Jane Smith", "Project Manager"),
+				new Employee("Alice Johnson", "Data Analyst")
+		);
+		employeeRepo.saveAll(employees);
+	}
 }
