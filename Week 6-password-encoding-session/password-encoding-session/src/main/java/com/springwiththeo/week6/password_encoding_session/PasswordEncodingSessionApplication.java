4c1ad686fb03a36e7e228e99eb2a8719dcf4da07
theolekan <tijesuolalekan@gmail.com>
2025-07-16 21:05:13 +0100

    Initialization of Week 6 Project

---
+++ Week 6-password-encoding-session/password-encoding-session/src/main/java/com/springwiththeo/week6/password_encoding_session/PasswordEncodingSessionApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week6.password_encoding_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncodingSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordEncodingSessionApplication.class, args);
+	}
+
+}
---
+++ Week 6-password-encoding-session/password-encoding-session/src/test/java/com/springwiththeo/week6/password_encoding_session/PasswordEncodingSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week6.password_encoding_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordEncodingSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
