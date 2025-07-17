8dfe89b1c33b90e0980dd0394c06022b3b8bc66d
theolekan <tijesuolalekan@gmail.com>
2025-07-17 17:35:24 +0100

    Implement delegating password encoder to support multiple encoding algorithms

--- Week 6-password-encoding-session/password-encoding-session/src/main/java/com/springwiththeo/week6/password_encoding_session/config/SecurityConfig.java
+++ Week 6-password-encoding-session/password-encoding-session/src/main/java/com/springwiththeo/week6/password_encoding_session/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.springwiththeo.week6.password_encoding_session.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,9 +11,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +36,23 @@ public class SecurityConfig implements CommandLineRunner {
 
     }
 
+
+    @Bean
+    public PasswordEncoder delegatingPasswordEncoder() {
+        String idForEncode = "argon2"; // use argon2 for new passwords
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
+    }
+
+    /**
+     * Replaced the below with delegating password encoder
+     * This allows us to use multiple password encoders and specify which one to use for new passwords.
+
     @Bean(name = "bcryptPasswordEncoder")
     PasswordEncoder bcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -43,15 +63,20 @@ public class SecurityConfig implements CommandLineRunner {
     @Bean(name = "argonPasswordEncoder")
     PasswordEncoder argonPasswordEncoder() {
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-    }
+    }*/
 
     @Bean
-    public UserDetailsService userDetailsService(@Qualifier("argonPasswordEncoder") PasswordEncoder passwordEncoder) {
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails adminUser = User.withUsername("admin")
                 .password(passwordEncoder.encode("admin123"))
                 .roles("ADMIN").build();
+
+        UserDetails oldUser = User.withUsername("old")
+                .password(new BCryptPasswordEncoder().encode("old123"))
+                .roles("USER").build();
+
         // In a real application, you would load users from a database or another source.
-        return new InMemoryUserDetailsManager(adminUser);
+        return new InMemoryUserDetailsManager(adminUser, oldUser);
     }
 
 
@@ -63,8 +88,6 @@ public class SecurityConfig implements CommandLineRunner {
         );
         // System.out.println("This is admin user's hashed password rehashed: " + bcryptPasswordEncoder().encode(userDetailsService(bcryptPasswordEncoder()).loadUserByUsername("admin").getPassword()) + "\nNotice that that they're different, this is because this is a one way hashing algorithm, and it will always generate a different hash for the same password input. This is a security feature to prevent rainbow table attacks.");
 
-        System.out.println("Argon2 Password Encoder: " + argonPasswordEncoder().encode("admin123"));
-        System.out.println("BCrypt Password Encoder: " + bcryptPasswordEncoder().encode("admin123"));
 
     }
 }
