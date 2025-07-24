42379a9c55282803b4dedc0dd24c11295dcf19e4
theolekan <tijesuolalekan@gmail.com>
2025-07-24 14:01:51 +0100

    Enhance session management in security configuration to limit concurrent sessions

--- Week 7-password-encoding-session part 2/password-encoding-session/src/main/java/com/springwiththeo/week7/password_encoding_session_p2/config/SecurityConfig.java
+++ Week 7-password-encoding-session part 2/password-encoding-session/src/main/java/com/springwiththeo/week7/password_encoding_session_p2/config/SecurityConfig.java
@@ -4,10 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,6 +43,10 @@ public class SecurityConfig implements CommandLineRunner {
                 .formLogin(form->{
                     form.successHandler(successHandler);
                     form.failureHandler(failureHandler);
+                })
+                .sessionManagement(session -> {
+                    session.maximumSessions(1).maxSessionsPreventsLogin(true);
+                    session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
                 })
                 .build();
 
