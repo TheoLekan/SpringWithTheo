056fb2a94b4dc0cfb2b6c3c4a7b81aabed032a09
theolekan <tijesuolalekan@gmail.com>
2025-07-28 09:41:41 +0100

    Initialize week 8 Spring Boot project with JWT authentication

---
+++ Week 8-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/DbJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week8.db_jwt_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbJwtAuthenticationApplication.class, args);
+	}
+
+}
---
+++ Week 8-db-jwt-authentication/db-jwt-authentication/src/test/java/com/springwiththeo/week8/db_jwt_authentication/DbJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week8.db_jwt_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
