57b77257996f108785360011346f273231e7e610
theolekan <tijesuolalekan@gmail.com>
2025-07-31 17:22:18 +0100

    Add custom authentication provider that used our userDetails and password encoder. And Configuring success handler in SecurityConfig

--- Week 8-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/SecurityConfig.java
+++ Week 8-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.springwiththeo.week8.db_jwt_authentication.configuratiion;
 
+import com.springwiththeo.week8.db_jwt_authentication.service.CustomerUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,10 +20,32 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomerUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomerUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService); // Set the custom user details service
+        provider.setPasswordEncoder(passwordEncoder()); // Set the password encoder
+        return provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(form->{
+                    form.successHandler((request, response,authentication)->{
+                        response.setStatus(200);
+                        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        response.getWriter().write("Login successful");
+                    });
+                }
+                )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated() // Require authentication for all other requests
                 )
