b8f33d2b6a0322c6c319b10e86a7372be54b2a60
theolekan <tijesuolalekan@gmail.com>
2025-08-04 14:15:32 +0100

    #16 Add role extraction from JWT in JwtAuthenticationFilter
    Here I'm taking the authorities from the JWT and using that to create the authenticated Object that will be added to the context. Rather than going to the database to get them
    
    Fixes #16

--- Week 9-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/JwtAuthenticationFilter.java
+++ Week 9-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/JwtAuthenticationFilter.java
@@ -7,12 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -35,11 +39,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (username!=null && SecurityContextHolder.getContext().getAuthentication()==null) {
             if (jwtService.isTokenValid(token)) {
                 UserDetails userDetails= userDetailsService.loadUserByUsername(username);
+
+                List<SimpleGrantedAuthority> roles = jwtService.extractRoles(token).stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
                 UsernamePasswordAuthenticationToken authToken=
                         new UsernamePasswordAuthenticationToken(
-                                userDetails,
+                                username,
                                 null,
-                                userDetails.getAuthorities()
+                                roles
                         );
 
                 SecurityContextHolder.getContext().setAuthentication(authToken);
