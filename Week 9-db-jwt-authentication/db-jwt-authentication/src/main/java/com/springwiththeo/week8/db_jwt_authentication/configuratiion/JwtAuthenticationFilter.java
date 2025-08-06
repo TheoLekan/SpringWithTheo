c804f35ba3bac6e538725cc67c3f8597bfe48715
theolekan <tijesuolalekan@gmail.com>
2025-08-06 19:24:31 +0100

    Adds extra Details like IP and session details

--- Week 9-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/JwtAuthenticationFilter.java
+++ Week 9-db-jwt-authentication/db-jwt-authentication/src/main/java/com/springwiththeo/week8/db_jwt_authentication/configuratiion/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -50,6 +51,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                 roles
                         );
 
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
 
             }
