c505a6c2ca6bcadf5fcf483352d969d362889393
theolekan <tijesuolalekan@gmail.com>
2025-09-04 15:17:22 +0100

    Initialize Week 13 Spring Data Access Project

---
+++ Week 13-Data-Access/src/main/java/com/springwiththeo/week13/data_access/SpringDataAccessProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week13.data_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccessProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAccessProjectApplication.class, args);
+	}
+
+}
