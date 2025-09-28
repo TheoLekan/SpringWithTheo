c45f383e38e185e4e69d7325b1419fe2a66d919e
theolekan <tijesuolalekan@gmail.com>
2025-09-28 21:34:54 +0100

    Create  Queries w/ QBE probes

--- Week 16 - Advance Spring Data JPA /jpa-advanced-queries/src/main/java/com/springwiththeo/week16/jpa_advanced_queries/service/BookService.java
+++ Week 16 - Advance Spring Data JPA /jpa-advanced-queries/src/main/java/com/springwiththeo/week16/jpa_advanced_queries/service/BookService.java
@@ -8,10 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,9 +23,8 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
-
     @Bean
-    public CommandLineRunner sampleMethod(){
+    public CommandLineRunner init(){
         return args -> {
             Author author = new Author("J.K. Rowling", "British");
             Author author2 = new Author("George R.R. Martin", "American");
@@ -32,6 +35,14 @@ public class BookService {
             Book book4 = new Book("A Clash of Kings", "Fantasy", LocalDate.of(1998, 11, 16), author2);
 
             bookRepository.saveAll(List.of(book1, book2, book3, book4));
+        };
+    }
+
+    @Bean
+    @DependsOn("init")
+    public CommandLineRunner sampleMethod(){
+        return args -> {
+
             bookRepository.findAll().forEach(System.out::println);
 
             bookRepository.findAll(BookSpefications.hasTitle("A Game of Thrones")).forEach(System.out::println);
@@ -51,6 +62,34 @@ public class BookService {
             System.out.print("Books by Rowling published between 1997 and 1999: ");
             List<Book> all = bookRepository.findAll(combinedSpec);
             all.forEach(System.out::println);
+        };
+    }
+
+
+    @Bean
+    @DependsOn("init")
+    public CommandLineRunner findByQBE(){
+        return args -> {
+            Book probe = new Book();
+            probe.setTitle("A Game of Thrones");
+
+            //Query by title
+            Optional<Book> resultsWithTitle = bookRepository.findOne(Example.of(probe, ExampleMatcher.matching().withIgnoreCase()));
+            resultsWithTitle.ifPresent(System.out::println);
+
+
+            // QueryByAuthorName
+            Book probe2=new Book();
+            Author authorProbe= new Author();
+            authorProbe.setName("Rowling");
+            probe2.setAuthor(authorProbe);
+            List<Book> all = bookRepository.findAll(Example.of(probe2, ExampleMatcher.matching().withIgnoreCase()
+                    .withMatcher("author.name", ExampleMatcher.GenericPropertyMatcher::contains))
+            );
+
+            all.forEach(System.out::println);
+
+
         };
     }
 
