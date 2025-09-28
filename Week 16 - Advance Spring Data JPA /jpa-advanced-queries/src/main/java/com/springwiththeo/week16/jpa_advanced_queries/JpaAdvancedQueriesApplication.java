006e65c768a230252bd4a8da89fd30b822908429
theolekan <tijesuolalekan@gmail.com>
2025-09-28 18:43:30 +0100

    Initialize JPA Advanced Queries project with Maven setup and basic configuration

---
+++ Week 16 - Advance Spring Data JPA /jpa-advanced-queries/src/main/java/com/springwiththeo/week16/jpa_advanced_queries/JpaAdvancedQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week16.jpa_advanced_queries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancedQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancedQueriesApplication.class, args);
+	}
+
+}
---
+++ Week 16 - Advance Spring Data JPA /jpa-advanced-queries/src/test/java/com/springwiththeo/week16/jpa_advanced_queries/JpaAdvancedQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week16.jpa_advanced_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancedQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
