4bedc80d09406e3ed2015efb7d42c8e5814034fd
theolekan <tijesuolalekan@gmail.com>
2025-10-04 22:15:31 +0100

    Init week 17 project

---
+++ Week 17 - Advance Spring Data JPA 2/advanced-jpa/src/main/java/com/springwiththeo/week17/advanced_jpa/AdvancedJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week17.advanced_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJpaApplication.class, args);
+	}
+
+}
