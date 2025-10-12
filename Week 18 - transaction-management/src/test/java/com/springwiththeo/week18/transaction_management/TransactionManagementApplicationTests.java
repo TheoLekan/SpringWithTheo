c668fa5fdcdcbecf875208779c6e26afb6efc5bc
theolekan <tijesuolalekan@gmail.com>
2025-10-12 18:59:29 +0100

    Initialize Spring Boot project

---
+++ Week 18 - transaction-management/src/test/java/com/springwiththeo/week18/transaction_management/TransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwiththeo.week18.transaction_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
